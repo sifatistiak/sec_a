b1bcf1c33f6c07557a640cb39ca73d4fef604191
SifatIstiak <sifatistiak@gmail.com>
2018-04-18 07:36:21 +0600

    aj ker commit

---
+++ src/com/test/seca/Student.java
@@ -0,0 +1,25 @@
+package com.test.seca;
+
+public class Student {
+    private String studentName;
+    private Integer studentId;
+
+    public Student() {
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+}
